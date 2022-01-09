7231f9ae71ff51c2aece6a247b44ade342ea3864
ktro2828 <ktro310115@gmail.com>
2022-01-09 15:07:54 +0900

    update build_transforms and bulild_heads to be loaded

--- dorec/models/__init__.py
+++ dorec/models/__init__.py
@@ -1,5 +1,5 @@
 from .backbones import HRNetV2, MobileNetV2, ResNet18, ResNet50, ResNet101, ResNext50, ResNext101
-from .builder import build_model
+from .builder import build_model, build_backbone, build_head
 from .base import ModuleBase, TaskModule
 from .heads import HeadBase, CNHead, DualHead, PPM, UPerNet
 from .modules import CAModule, EDModule
@@ -8,7 +8,7 @@ from .unet import UNet, NestedUNet
 
 
 __all__ = (
-    "build_model",
+    "build_model", "build_backbone", "build_head",
     "HRNetV2", "MobileNetV2",
     "ResNet18", "ResNet50", "ResNet101",
     "ResNext50", "ResNext101",
